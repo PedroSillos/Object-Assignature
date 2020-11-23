8e4da941ba3ae90d9d9f1b68365b4eca2ec37373
Pedro Sillos <pedro.sillos@gmail.com>
2020-11-23 16:36:21 -0300

    started again

--- Color Model Converter/Color Model Converter/Form1.Designer.cs
+++ Color Model Converter/Color Model Converter/Form1.Designer.cs
@@ -30,23 +30,16 @@
         {
             this.picBox_rgb1 = new System.Windows.Forms.PictureBox();
             this.btn_buscar_imagem = new System.Windows.Forms.Button();
-            this.picBox_rgb2 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btn_rgb1_para_yiq_para_rgb2 = new System.Windows.Forms.Button();
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.numUD_constante = new System.Windows.Forms.NumericUpDown();
+            this.btn_achar_assinatura = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBox_rgb1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picBox_rgb2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numUD_constante)).BeginInit();
             this.SuspendLayout();
             // 
             // picBox_rgb1
             // 
-            this.picBox_rgb1.Location = new System.Drawing.Point(140, 45);
+            this.picBox_rgb1.Location = new System.Drawing.Point(198, 69);
+            this.picBox_rgb1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.picBox_rgb1.Name = "picBox_rgb1";
-            this.picBox_rgb1.Size = new System.Drawing.Size(360, 360);
+            this.picBox_rgb1.Size = new System.Drawing.Size(309, 310);
             this.picBox_rgb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBox_rgb1.TabIndex = 0;
             this.picBox_rgb1.TabStop = false;
@@ -54,119 +47,42 @@
             // btn_buscar_imagem
             // 
             this.btn_buscar_imagem.AutoSize = true;
-            this.btn_buscar_imagem.Location = new System.Drawing.Point(12, 12);
+            this.btn_buscar_imagem.Location = new System.Drawing.Point(18, 19);
+            this.btn_buscar_imagem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_buscar_imagem.Name = "btn_buscar_imagem";
-            this.btn_buscar_imagem.Size = new System.Drawing.Size(115, 27);
+            this.btn_buscar_imagem.Size = new System.Drawing.Size(172, 42);
             this.btn_buscar_imagem.TabIndex = 2;
             this.btn_buscar_imagem.TabStop = false;
             this.btn_buscar_imagem.Text = "Buscar imagem";
             this.btn_buscar_imagem.UseVisualStyleBackColor = true;
             this.btn_buscar_imagem.Click += new System.EventHandler(this.btn_buscar_imagem_Click);
             // 
-            // picBox_rgb2
+            // btn_achar_assinatura
             // 
-            this.picBox_rgb2.Location = new System.Drawing.Point(696, 45);
-            this.picBox_rgb2.Name = "picBox_rgb2";
-            this.picBox_rgb2.Size = new System.Drawing.Size(360, 360);
-            this.picBox_rgb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picBox_rgb2.TabIndex = 4;
-            this.picBox_rgb2.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(289, 408);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "RGB 1";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(717, 408);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(320, 20);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "RGB1 - RGB 2 ou RGB2 com mais brilho";
-            // 
-            // btn_rgb1_para_yiq_para_rgb2
-            // 
-            this.btn_rgb1_para_yiq_para_rgb2.AutoSize = true;
-            this.btn_rgb1_para_yiq_para_rgb2.Location = new System.Drawing.Point(189, 12);
-            this.btn_rgb1_para_yiq_para_rgb2.Name = "btn_rgb1_para_yiq_para_rgb2";
-            this.btn_rgb1_para_yiq_para_rgb2.Size = new System.Drawing.Size(270, 27);
-            this.btn_rgb1_para_yiq_para_rgb2.TabIndex = 8;
-            this.btn_rgb1_para_yiq_para_rgb2.TabStop = false;
-            this.btn_rgb1_para_yiq_para_rgb2.Text = "RGB1 para YIQ para RGB2 e subtração";
-            this.btn_rgb1_para_yiq_para_rgb2.UseVisualStyleBackColor = true;
-            this.btn_rgb1_para_yiq_para_rgb2.Click += new System.EventHandler(this.btn_rgb1_para_yiq_para_rgb2_Click);
-            // 
-            // btn_rgb1_para_yiq_com_brilho_para_rgb2
-            // 
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.AutoSize = true;
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.Location = new System.Drawing.Point(690, 12);
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.Name = "btn_rgb1_para_yiq_com_brilho_para_rgb2";
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.Size = new System.Drawing.Size(371, 27);
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.TabIndex = 9;
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.TabStop = false;
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.Text = "RGB1 para YIQ com aumento de luminância para RGB2";
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.UseVisualStyleBackColor = true;
-            this.btn_rgb1_para_yiq_com_brilho_para_rgb2.Click += new System.EventHandler(this.btn_rgb1_para_yiq_com_brilho_para_rgb2_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(514, 55);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(168, 17);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "Clarear ou escurecer em:";
-            // 
-            // numUD_constante
-            // 
-            this.numUD_constante.Increment = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
-            this.numUD_constante.Location = new System.Drawing.Point(539, 75);
-            this.numUD_constante.Maximum = new decimal(new int[] {
-            300,
-            0,
-            0,
-            0});
-            this.numUD_constante.Minimum = new decimal(new int[] {
-            300,
-            0,
-            0,
-            -2147483648});
-            this.numUD_constante.Name = "numUD_constante";
-            this.numUD_constante.Size = new System.Drawing.Size(120, 22);
-            this.numUD_constante.TabIndex = 10;
+            this.btn_achar_assinatura.AutoSize = true;
+            this.btn_achar_assinatura.Location = new System.Drawing.Point(198, 19);
+            this.btn_achar_assinatura.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_achar_assinatura.Name = "btn_achar_assinatura";
+            this.btn_achar_assinatura.Size = new System.Drawing.Size(309, 42);
+            this.btn_achar_assinatura.TabIndex = 8;
+            this.btn_achar_assinatura.TabStop = false;
+            this.btn_achar_assinatura.Text = "Achar assinatura";
+            this.btn_achar_assinatura.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1203, 452);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.numUD_constante);
-            this.Controls.Add(this.btn_rgb1_para_yiq_com_brilho_para_rgb2);
-            this.Controls.Add(this.btn_rgb1_para_yiq_para_rgb2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.picBox_rgb2);
+            this.ClientSize = new System.Drawing.Size(549, 412);
+            this.Controls.Add(this.btn_achar_assinatura);
             this.Controls.Add(this.btn_buscar_imagem);
             this.Controls.Add(this.picBox_rgb1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.picBox_rgb1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picBox_rgb2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numUD_constante)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,13 +92,7 @@
 
         private System.Windows.Forms.PictureBox picBox_rgb1;
         private System.Windows.Forms.Button btn_buscar_imagem;
-        private System.Windows.Forms.PictureBox picBox_rgb2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btn_rgb1_para_yiq_para_rgb2;
-        private System.Windows.Forms.Button btn_rgb1_para_yiq_com_brilho_para_rgb2;
-        private System.Windows.Forms.Label label2;
-        public System.Windows.Forms.NumericUpDown numUD_constante;
+        private System.Windows.Forms.Button btn_achar_assinatura;
     }
 }
 
--- Color Model Converter/Color Model Converter/Properties/AssemblyInfo.cs
+++ Color Model Converter/Color Model Converter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Color Model Converter")]
+[assembly: AssemblyTitle("Object Assignature")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Color Model Converter")]
+[assembly: AssemblyProduct("Object Assignature")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
